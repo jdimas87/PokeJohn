ed22074063abb1ac7b0e73ea26f3356fc6ae9215
John Dimas <47593219+jdimas87@users.noreply.github.com>
2022-07-17 22:22:11 +0300

    Adds layout component setup

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -1,3 +1,19 @@
-const Layout = () => {};
+import { Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from '../UI/header';
+import Footer from '../UI/footer';
+
+const Layout = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Container>
+        <Outlet />
+      </Container>
+      <Footer />
+    </Fragment>
+  );
+};
 
 export default Layout;
