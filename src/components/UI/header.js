6803d720c59e909d24a3940154fce05d10ee7819
John Dimas <47593219+jdimas87@users.noreply.github.com>
2022-07-17 22:56:06 +0300

    Adds header component html and styles

--- src/components/UI/header.js
+++ src/components/UI/header.js
@@ -1,3 +1,19 @@
-const Header = () => {};
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import styles from '../../assets/css/header.module.css';
+
+const Header = () => {
+  return (
+    <Container fluid className={`${styles.header} py-5 text-center`}>
+      <Row>
+        <Col>
+          <Link to='/'>
+            <h1>PokeJohn</h1>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Header;
