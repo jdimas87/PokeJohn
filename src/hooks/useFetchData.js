532dc4a953be49cbb4737a460b9eca150eeadbe4
John Dimas <47593219+jdimas87@users.noreply.github.com>
2022-07-20 00:13:09 +0300

    Adds useFetchData custom hook

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetchData = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
