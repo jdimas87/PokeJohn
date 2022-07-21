5cc17a862b153de57b01c8c510fd76aeb3d8dd78
John Dimas <47593219+jdimas87@users.noreply.github.com>
2022-07-21 20:56:37 +0300

    Adds pokemons component html and styles

--- src/pages/pokemons.js
+++ src/pages/pokemons.js
@@ -1,3 +1,44 @@
-const Pokemons = () => {};
+import { useState, useEffect } from 'react';
+import { Spinner } from 'react-bootstrap';
+import useFetchData from '../hooks/useFetchData';
+import PokemonToast from '../components/UI/pokemonToast';
+import styles from '../assets/css/pokemons.module.css';
+
+const Pokemons = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  const { data, error } = useFetchData('https://pokeapi.co/api/v2/pokemon');
+
+  useEffect(() => {
+    if (data) {
+      setPokemons(data.results);
+      setIsLoading(false);
+    }
+
+    if (error) {
+      setHasError(true);
+    }
+  }, [data, error]);
+
+  if (isLoading) {
+    return (
+      <div className={styles.spinner_wrapper}>
+        <Spinner
+          animation='grow'
+          variant='danger'
+          style={{ width: '10rem', height: '10rem' }}
+        />
+      </div>
+    );
+  } else if (hasError) {
+    return <h3>Oops. Something Happend.</h3>;
+  } else {
+    return pokemons.map((pokemon) => (
+      <PokemonToast name={pokemon.name} url={pokemon.url} />
+    ));
+  }
+};
 
 export default Pokemons;
