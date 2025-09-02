e3279ad96aa61360319b822d1fdc382e93a34135
John Dimas <ioannisdimas87@gmail.com>
2025-09-02 17:22:55 +0300

    Adds pokemon info page & offset/limit at fetch pokemons request

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -6,7 +6,7 @@ import PokemonsLayout from '../components/layouts/PokemonsLayout';
 const Pokemons = () => {
   const params = useParams();
 
-  const { data: pokemons } = useSwr('/pokemon');
+  const { data: pokemons } = useSwr('/pokemon?offset=0&limit=20');
 
   return <PokemonsLayout pokemons={pokemons.data.results} params={params} />;
 };
